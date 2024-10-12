f6e1c72a62b0f98a3ddde83650675b50586f0c6e
Kirill <syskirill.com>
2024-10-12 23:05:46 +0300

    Initial Commit

---
+++ TestDelegateApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestDelegateApp2
+//
+//  Created by Кирилл Сысоев on 12.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
